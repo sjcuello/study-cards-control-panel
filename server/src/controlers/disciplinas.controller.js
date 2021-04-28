c14586c0a8ea3a44bd41bf7aebd115572431853e
Santiago Cuello <santiago.cuello@ross.com.ar>
2021-04-27 23:44:21 -0300

    Create and test first controller

---
+++ server/src/controlers/disciplinas.controller.js
@@ -0,0 +1,4 @@
+export function createProject(req, res){
+    console.log('req.body :>> ', req.body);
+    res.send('recieved')
+}
--- server/src/routes/disciplinas.js
+++ server/src/routes/disciplinas.js
@@ -1,4 +1,7 @@
 import { Router } from 'express'
+import { createProject } from '../controlers/disciplinas.controller'
 const router = Router();
+
+router.get('/', createProject)
 
 export default router;
