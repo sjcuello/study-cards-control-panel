a90c1aae7067efae6dbff7431ad94824074d61a8
sjcuello <sjcuello@gmail.com>
2021-05-01 23:43:05 -0300

    Basic routing

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { ConfigurationComponent } from './configuration/configuration.component'
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'configuration',
+    component: ConfigurationComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
