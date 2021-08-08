b1d56f11c6dbcf9e5ab8afc0ea0fc5c8d64549f9
Santiago Cuello <sjcuello@gmail.com>
2021-08-08 20:42:42 -0300

    Mongo connection

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -5,9 +5,9 @@ dotenv.config();
 export const config = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 3000,
-    CORS: process.env.CORS,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_HOST: process.env.DB_HOST,
-    DB_NAME: process.env.DB_NAME,
+    cors: process.env.CORS,
+    db_user: process.env.DB_USER,
+    db_password: process.env.DB_PASSWORD,
+    db_host: process.env.DB_HOST,
+    db_name: process.env.DB_NAME,
 }
---
+++ server/src/lib/mongo.js
@@ -0,0 +1,62 @@
+import { MongoClient, ObjectId } from 'mongodb'
+import { config } from '../config/index'
+
+const USER = encodeURIComponent(config.db_user)
+const PASSWORD = encodeURIComponent(config.db_password)
+const NAME = encodeURIComponent(config.db_name)
+const HOST = encodeURIComponent(config.db_host)
+
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${HOST}/${NAME}?retryWrites=true&w=majority`
+
+export class MongoLib {
+    constructor() {
+        this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+        this.dbName = NAME;
+    }
+
+    connect() {
+        if (!MongoLib.connection) {
+            MongoLib.connection = new Promise((resolve, reject) => {
+                this.client.connect(err => {
+                    if (err) {
+                        reject(err)
+                    }
+                    console.log('Connected succesfully to mongo !');
+                    resolve(this.client.db(this.dbName))
+                });
+            });
+        }
+        return MongoLib.connection;
+    }
+
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db.collection(collection).find(query).toArray();
+        })
+    }
+
+    get(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).findOne({ _id: ObjectId(id) }).toArray();
+        })
+    }
+
+    create(collection, data) {
+        return this.connect().then(db => {
+            return db.collection(collection).insertOne(data).toArray();
+        }).then(result => result.insertId);
+    }
+
+    update(collection, id, data) {
+        return this.connect().then(db => {
+            return db.collection(collection).updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+        }).then(result => result.upsertId || id);
+    }
+
+    delete(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).deleteOne({ _id: ObjectId(id) }).toArray();
+        }).then(() => id);
+    }
+}
