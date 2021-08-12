f8d39ee602d44125349b542ed302fd45767ae273
Santiago Cuello <sjcuello@gmail.com>
2021-08-12 20:55:10 -0300

    Delete sequelize and old models mysql

---
+++ server/src/models/allModels.js
@@ -0,0 +1,37 @@
+export const Disciplinas = {
+    _id,
+    nombre,
+    color,
+    estado
+}
+
+export const Tar_contenidos = {
+    _id,
+    id_tarjeta,
+    contenido,
+    estado
+}
+
+export const Tar_ejemplos = {
+    _id,
+    id_tarjeta,
+    ejemplo,
+    estado
+}
+
+export const Tarjetas = {
+    _id,
+    id_tema_estudio,
+    titulo,
+    color,
+    estado
+}
+
+export const TemasEstudio = {
+    id,
+    id_disciplina,
+    nombre,
+    color,
+    tag,
+    estado
+}
